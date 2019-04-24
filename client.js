ea04bdf3510532a94a961a48912da587ed27c5c5
otsubo-toru <otsubo.toru@gmail.com>
2019-04-25 01:54:47 +0900

    Updates

--- client.js
+++ client.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useRouterHistory } from "react-router";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createHistory, useBasename } from "history";
 
 import Layout from "./layouts/Layout";
 import Top from "./pages/Top";
 import Works from "./pages/Works";
 
+const history = useBasename(createHistory)({
+  basename: "/portfolio-react/"
+});
+
 const app = document.getElementById('app');
 
-// ReactDOM.render(<Layout/>, app);
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <Layout>
       <Route exact path="/" component={Top}></Route>
       <Route path="/works" name="works" component={Works}></Route>
