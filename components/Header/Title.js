c7f7e598a9662e68cc510c4d43347598246761d8
otsubo-toru <otsubo.toru@gmail.com>
2019-04-25 13:29:40 +0900

    Updates

--- components/Header/Title.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Title extends React.Component {
-  render() {
-    return (
-      <h1 class="site-name">{this.props.title}</h1>
-    );
-  }
-}
---
+++ precache-manifest.d54d61864f43cf8e919003d7ea7924f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77f1142330b275dbc1ac62666aa33d5",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "59739852e7364fd7b1c2",
+    "url": "/portfolio-react/static/js/2.b823d8d4.chunk.js"
+  },
+  {
+    "revision": "52a55b53a17c7bffcc68",
+    "url": "/portfolio-react/static/js/main.6b7b5d34.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  }
+]);
